7d087158c425f166bf39297431d4f21f7d59c1ba
idsanchezf <156478003+idsanchezf@users.noreply.github.com>
2025-03-28 22:54:32 +0000

    Agrego microservicio para el lab

---
+++ src/test/java/com/eci/arcn/microservice_lab/MicroserviceLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.arcn.microservice_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
